a0b4e40207d400e519e50340290f6341f67e7de8
James Shobbrook <jamesshobbrook@hotmail.co.uk>
2020-02-16 13:57:33 -0800

    Changed Nav Bar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,13 +7,36 @@
 
 
 
+
 </head>
 <body>
 	<div id="header" class="container">
-		<div id="logo">
-			<h1><a href="/">National University Sports</a></h1>
-		</div>
+		
 		<div>
+
+			<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+			  <a class="navbar-brand" href="/">National University Sports</a>
+			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+			    <span class="navbar-toggler-icon"></span>
+			  </button>
+			  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+			    <ul class="navbar-nav">
+			      <li class="nav-item">
+			        <a class="nav-link" href="/sports">Sports</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="/fixtures">Fixtures</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="/about">About</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="/myTeam">My Team</a>
+			      </li>    
+			    </ul>
+			  </div>  
+			</nav>
+			<!--
 			<ul class="nav">
 				<li class="btn btn-outline-primary" class="{{ Request::path() == '/' ? 'current_page_item' : ''}}"><a class="nav-link" href="/" accesskey="1" title="">Homepage</a></li>
 
@@ -22,7 +45,7 @@
 				<li class="btn btn-outline-primary" class="{{ Request::path() == 'about' ? 'current_page_item' : ''}}"><a class="nav-link" href="/about" accesskey="3" title="">About Us</a></li>
 				<li class="btn btn-outline-primary" class="{{ Request::path() == '/' ? 'current_page_item' : ''}}"><a class="nav-link" href="#" accesskey="4" title="">Careers</a></li>
 				<li class="btn btn-outline-primary" class="{{ Request::path() == 'myTeam' ? 'current_page_item' : ''}}"><a class="nav-link" href="/myTeam" accesskey="5" title="">My Team</a></li>
-			</ul>
+			</ul>-->
 		</div>
 	</div>
 @yield ('content')
--- resources/views/myTeam.blade.php
+++ resources/views/myTeam.blade.php
@@ -1,1 +1,12 @@
 @extends ('layout')
+
+@section('content')
+<div class="container">
+	<div class ="row-border border-primary-rounded" style="border: 1px solid black">asdfasdfasf</div>
+	<div class ="row"> </div>
+	<div class ="row-border border-primary-rounded" style="border: 1px solid black">asdfasdfasf</div>
+
+
+</div>
+
+@endsection
